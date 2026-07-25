bceca529975d4b40bcf53121807830ae89e9d45d
Diksha Bharti <bhartidiksha2008@gmail.com>
2026-07-25 11:17:57 +0530

    Add: count-paths - submission-4

---
+++ Data Structures & Algorithms/count-paths/submission-4.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    int totalPath(int i, int j, int r, int c,vector<vector<int>>& dp){
+        if(i == r && j == c){
+            return 1; // base case (destination is reached)
+        }
+
+        if(dp[i][j] != -1) return dp[i][j];
+
+        int right = ((i+1 <= r) ? totalPath(i+1,j,r,c,dp) : 0);
+        int down = ((j+1 <= c) ? totalPath(i,j+1,r,c,dp) : 0);
+        return dp[i][j] = right+down;
+    }
+    int uniquePaths(int m, int n) {
+        if(m == 1 && n == 1) return 1;
+        /*
+        vector<vector<int>> dp(m+1,vector<int>(n+1,-1));
+
+        for(int i = 0; i < n; i++)
+            dp[m][i] = 1;
+        
+        for(int j = 0; j < m; j++)
+            dp[j][n] = 1;
+
+        for(int i = m-1; i >= 1; i--){
+            for(int j = n-1; j >= 1; j--){
+                int right = 0, down = 0;
+                // right dir total ways
+                if(i+1 <= m) right = dp[i+1][j];
+                // down dir total ways
+                if(j+1 <= n) down = dp[i][j+1];
+
+                dp[i][j] = right+down;
+            }
+        }
+        // return totalPath(1,1,m,n,dp);
+        return dp[1][1];
+        */
+
+        vector<int> dp(n+1,1);
+        for(int i = m-1; i >= 1; i--){
+            vector<int> temp(n+1,1);
+            for(int j = n-1; j >= 1; j--){
+                temp[j] = dp[j] + temp[j+1];
+            }
+            dp = temp;
+        }
+        return dp[1];
+    }
+};
