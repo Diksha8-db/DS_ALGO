d2f1e3e550967bd38b360fd1eba9134a03d84fce
Diksha Bharti <bhartidiksha2008@gmail.com>
2026-08-22 11:08:14 +0530

    Add: kth-largest-integer-in-a-stream - submission-0

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-0.cpp
@@ -0,0 +1,22 @@
+class KthLargest {
+public:
+    priority_queue<int,vector<int>, greater<int>> pq;
+    int maxValidSize;
+    KthLargest(int k, vector<int>& nums) {
+        maxValidSize = k;
+        for(int num : nums){
+            pq.push(num);
+            if(pq.size() > k){
+                pq.pop();
+            }
+        }
+    }
+    
+    int add(int val) {
+        pq.push(val);
+        while(pq.size() > maxValidSize)
+            pq.pop();
+        
+        return pq.top();
+    }
+};
